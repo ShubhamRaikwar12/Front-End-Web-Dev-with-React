2cb9465311d377aee990aa121db3d86d8fd2a185
unknown <raikwarshubham433@gmail.com>
2021-12-25 20:26:36 +0530

    controlled and uncontrolled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,3 +1,5 @@
+// Author: @Shubham Raikwar
+
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
@@ -8,6 +10,8 @@ import '../App.css';
 import Home from './HomeComponent';
 import {Route, Redirect, Switch } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
+
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -26,17 +30,37 @@ class Main extends Component {
 
 }
 
-
   render(){
-
-    const HomePage = () =>{
+    const HomePage =() =>{
       return(
-        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
-        promotion={this.state.promotions.filter((promo) => promo.featured)[0]} 
-        leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+        <Home 
+            dish ={this.state.dishes.filter((dish)=>dish.featured)[0]}
+            promotion={this.state.promotions.filter((promotion)=>promotion.featured)[0]}
+            leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
         />
       );
-    }
+    };
+  
+
+  const AboutUsPage = () =>{
+    return(
+        <About
+          leaders={this.state.leaders}
+        />
+    );
+  };
+
+  const DishWithId = ({match}) =>{
+    return(
+      <DishDetail
+        dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+      />
+    );
+  };
+
+
+  
   return (
     <div>
       <Header />
@@ -45,7 +69,9 @@ class Main extends Component {
           <Route path="/home" component={HomePage} /> 
           {/* Route to MENU */}
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={ AboutUsPage } />
           {/* Default path if above does not gets executed */}
           <Redirect to="/home"/>
         </Switch>
